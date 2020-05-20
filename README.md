db04c4c333eb9f2e8ed9d9f0d80976cef6d88790
ErrorMessage404 <56453471+ErrorMessage404@users.noreply.github.com>
2020-05-20 11:14:56 -0400

    Stage 1

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # Easy-HypixelAPI
 An easy way to utilize the HypixelAPI in node.js
 
+[![NPM Badge](https://nodei.co/npm/hypixie.png)](https://npmjs.com/package/hypixie)
 
-- Your name 
+## Install
 
-- Project title
+```sh
+npm install hypixie
+```
 
-- Links to website & social media
+## Usage
 
-- Contact details
+```js
+const hypixie = require("hypixie");
+
+(async () => {
+	const { displayname } = await hypixie("player", {
+		uuid: "56da43a4-088d-4a76-82b6-dd431535015e",
+		key: "some api key"
+	});
+
+	console.log(displayname);
+	//=> "Richienb"
+})();
+```
--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-class Color {
-    constructor(name, code) {
-        this.name = name;
-        this.code = code;
-      }
+const fetch = require('cross-fetch');
+
+class Hypixel {
+	/**
+	* Create a new API client instance.
+	* @param {string} key - A valid Hypixel API key
+	*/
+    constructor (key) {
+		this.key = key
+    }
+
+    /**
+	* Get a player's UUID.
+	* @param {string} username - The target user's ingame name
+	*/
+    async getUUID(username) {
+        if(!username) throw new Error("Missing arguments");
+        let data;
+        await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+        .then(res => {
+          if (res.status >= 400) {
+            data = null
+            throw new Error("Bad response from server");
+          }
+          return res.json();
+        })
+        .then(d => { data = d; })
+        .catch(err => { console.error(err); data = null; });
+        return data;
+    }
+    
+    async playerCount() {
+        let data;
+        await fetch(`https://api.hypixel.net/playerCount?key=${this.key}`)
+        .then(res => {
+        if (res.status >= 400) {
+            data = null
+            throw new Error("Bad response from server");
+        }
+        return res.json();
+        })
+        .then(d => { data =  d; })
+        .catch(err => { console.error(err); data = null; });
+        return data;
+    }
+    
+    async playerFromUUID(uuid) {
+        let data;
+        await fetch(`https://api.hypixel.net/player?uuid=${uuid}&key=${this.key}`)
+        .then(res => {
+        if (res.status >= 400) {
+            data = null
+            throw new Error("Bad response from server");
+        }
+        return res.json();
+        })
+        .then(d => { data = d; })
+        .catch(err => { console.error(err); data = null; });
+        return data;
+    }
+
+    async playerFromUsername(username) {
+        let data;
+        let uuid = await this.getUUID(username).then(res => { return res.id })
+        await fetch(`https://api.hypixel.net/player?uuid=${uuid}&key=${this.key}`)
+        .then(res => {
+        if (res.status >= 400) {
+            data = null
+            throw new Error("Bad response from server");
+        }
+        return res.json();
+        })
+        .then(d => { data = d; })
+        .catch(err => { console.error(err); data = null; });
+        return data;
+    }
 }
 
-const allColors = [
-    new Color('brightred', '#E74C3C'),
-    new Color('soothingpurple', '#9B59B6'),
-    new Color('skyblue', '#5DADE2'),
-    new Color('leafygreen', '#48C9B0'),
-    new Color('sunkissedyellow', '#F4D03F'),
-    new Color('groovygray', '#D7DBDD'),
-];
+module.exports = Hypixel
 
-exports.getRandomColor = () => {
-  return allColors[Math.floor(Math.random() * allColors.length)];
-}
-
-exports.allColors = allColors;
